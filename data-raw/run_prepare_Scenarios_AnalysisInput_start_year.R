1493114f7f404f2ce0616950325dbff36a0ce118
Franziska Fischer <110397916+frmfisc@users.noreply.github.com>
2022-11-29 10:34:50 +0100

    Update to geco 2021 (#15)
    * added new GECO2021
    
    * add file
    
    * prepare file

--- data-raw/run_prepare_Scenarios_AnalysisInput_start_year.R
+++ data-raw/run_prepare_Scenarios_AnalysisInput_start_year.R
@@ -2,13 +2,13 @@ devtools::load_all()
 
 # make sure to set the relevant start year when running the data preparation
 # example:
-# start_year <- 2021
+ start_year <- 2021
 input_path <- r2dii.utils::path_dropbox_2dii(
   "PortCheck",
   "00_Data",
   "01_ProcessedData",
   "03_ScenarioData",
-  glue::glue("Scenarios_AnalysisInput_{start_year}.csv")
+  glue::glue("new_Scenarios_AnalysisInput_{start_year}.csv")
 )
 
 data <- readr::read_csv(
@@ -35,26 +35,7 @@ data <- readr::read_csv(
 
 prepared_data <- prepare_scenario_data(data = data, start_year = start_year)
 
-# due to likely manual errors or alike developing economies and advanced
-# economies have different name formatting from different sectors in 2020 data.
-# Fixing this here in a hardcoded way.
-if (start_year == 2020) {
-  prepared_data <- prepared_data %>%
-    dplyr::mutate(
-      scenario_geography = dplyr::if_else(
-        .data$scenario_geography == "Advancedeconomies",
-        "AdvancedEconomies",
-        .data$scenario_geography
-      )
-    ) %>%
-    dplyr::mutate(
-      scenario_geography = dplyr::if_else(
-        .data$scenario_geography == "Developingeconomies",
-        "DevelopingEconomies",
-        .data$scenario_geography
-      )
-    )
-}
+
 # due to likely manual errors the raw input scenario data for start year 2020
 # versus 2021, there are slight formatting differences in a scenario geography
 # Fixing this here in a hardcoded way.
@@ -66,7 +47,34 @@ if (start_year == 2021) {
         "CentralandSouthAmerica",
         .data$scenario_geography
       )
-    )
+    ) %>%
+    dplyr::mutate(
+      scenario_geography = dplyr::if_else(
+        .data$scenario_geography == "Emerging market and developing economies",
+        "Emergingmarket&developingeconomies",
+        .data$scenario_geography
+      )
+    ) %>%
+    dplyr::mutate(
+      scenario_geography = dplyr::if_else(
+        .data$scenario_geography == "EU27",
+        "EU",
+        .data$scenario_geography
+      )
+    ) %>%
+    dplyr::mutate(
+      scenario_geography = dplyr::if_else(
+        .data$scenario_geography == "Advanced economies",
+        "AdvancedEconomies",
+        .data$scenario_geography
+      ))
+}
+
+#filter for the automotive sector for the GECO data
+if (start_year == 2021) {
+geco_power_sector <- prepared_data %>% filter(str_detect(scenario, 'GECO')) %>% filter(ald_sector != "Automotive")
+
+prepared_data <- prepared_data %>% anti_join(geco_power_sector)
 }
 
 prepared_data %>% readr::write_csv(
