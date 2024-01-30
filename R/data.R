4e47cc97b2df1502fbff9c4c277ddb4f1b073f60
Bertrand <62471481+bergalli@users.noreply.github.com>
2024-01-30 23:01:23 +0100

    add open source data as part of package (#110)

--- R/data.R
+++ R/data.R
@@ -1,14 +1,35 @@
-#' Available production types in the stress test
-#'
-#' @source internal
-"production_types"
+# EXPORTED DATASETS ========================================
 
+#' master Scenarios_AnalysisInput used in the stress test
+#' @source see generative script in tests folder
+"Scenarios_AnalysisInput"
+
+#' master price_data_long used in the stress test
+#' @source see generative script in tests folder
+"price_data_long"
+
+#' master prewrangled_capacity_factors used in the stress test
+#' @source see generative script in tests folder
+"prewrangled_capacity_factors"
+
+#' master ngfs_carbon_price used in the stress test
+#' @source see generative script in tests folder
+"ngfs_carbon_price"
+
+# PREPROCESSING DATASETS ========================================
 
 #' available geographies in the stress test
 #'
 #' @source internal
 "scenarios_geographies"
 
+
+# MOCK DATASETS ========================================
+
+#' Used to generate company synthetic datasets
+#' TODO remove to be 
+#' @source Scenario analysis input available production types
+"production_types"
 
 
 #' GENERATE COMPANY ACTIVITY
@@ -24,4 +45,3 @@
 #' generated
 #' @source see generative script in tests folder
 "synthetic_eikon_data"
-
--- data-raw/run_workflow.R
+++ data-raw/run_workflow.R
@@ -1,6 +1,8 @@
 start_year <- 2022
 
-print("=================== RUNNING run_prepare_Scenarios_AnalysisInput_start_year ===================")
+# OPEN SOURCE DATA
+
+print("=================== RUNNING run_prepare_Scenarios_AnalysisInput ===================")
 source(fs::path("data-raw", "run_prepare_Scenarios_AnalysisInput.R"))
 rm(list = ls()[ls() != "start_year"])
 
@@ -20,6 +22,27 @@ print("=================== RUNNING run_rename_geographies ===================")
 source(fs::path("data-raw", "run_rename_geographies.R"))
 rm(list = ls()[ls() != "start_year"])
 
+
+
+# ===== SAVE AS PACKAGE DATA
+
+Scenarios_AnalysisInput <- readr::read_csv(here::here("data-raw", "st_inputs", "Scenarios_AnalysisInput.csv"))
+usethis::use_data(Scenarios_AnalysisInput, overwrite = TRUE)
+
+prewrangled_capacity_factors <- readr::read_csv(here::here("data-raw", "st_inputs", "prewrangled_capacity_factors.csv"))
+usethis::use_data(prewrangled_capacity_factors, overwrite = TRUE)
+
+price_data_long <- readr::read_csv(here::here("data-raw", "st_inputs", "price_data_long.csv"))
+usethis::use_data(price_data_long, overwrite = TRUE)
+
+ngfs_carbon_price <- readr::read_csv(here::here("data-raw", "st_inputs", "ngfs_carbon_price.csv"))
+usethis::use_data(ngfs_carbon_price, overwrite = TRUE)
+
+
+
+# CLOSED SOURCE DATA
+
+
 print("=================== RUNNING run_prepare_abcd_stress_test_input ===================")
 source(fs::path("data-raw", "run_prepare_abcd_stress_test_input.R"))
 rm(list = ls()[ls() != "start_year"])
@@ -28,12 +51,14 @@ print("=================== RUNNING run_prepare_prewrangled_financial_data_stress
 source(fs::path("data-raw", "run_prepare_prewrangled_financial_data_stress_test.R"))
 rm(list = ls()[ls() != "start_year"])
 
-
 # ===== SAVE TO DROPBOX
 
-for (fp in list.files(here::here("data-raw", "st_inputs"))){
+for (fp in c("abcd_stress_test_input.csv","financial_data_stress_test.csv")){
+  
   readr::write_csv(
     readr::read_csv(here::here("data-raw", "st_inputs", fp)),
     r2dii.utils::path_dropbox_2dii(fs::path("ST Inputs", "ST_INPUTS_MASTER", fp))
   )
+
 }
+
