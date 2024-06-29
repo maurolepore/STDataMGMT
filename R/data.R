dc11750920232d1547fde05367c2b09ccf48b44a
Mauro Lepore <maurolepore@gmail.com>
2024-06-29 00:08:34 +0000

    Add examples for datasets

--- R/data.R
+++ R/data.R
@@ -1,27 +1,37 @@
 #' available geographies in the stress test
 #'
 #' @source internal
+#' @examples
+#' scenarios_geographies
 "scenarios_geographies"
 
 
 # MOCK DATASETS ========================================
 
 #' Used to generate company synthetic datasets
-#' TODO remove to be 
+#' TODO remove to be
 #' @source Scenario analysis input available production types
+#' @examples
+#' production_types
 "production_types"
 
 
 #' GENERATE COMPANY ACTIVITY
 #' @source see generative script in tests folder
+#' @examples
+#' synthetic_company_activities
 "synthetic_company_activities"
 
 #' GENERATE COMPANY EMISSIONS
 #' generated from company_activities
 #' @source see generative script in tests folder
+#' @examples
+#' synthetic_company_emissions
 "synthetic_company_emissions"
 
 
 #' generated
 #' @source see generative script in tests folder
+#' @examples
+#' synthetic_eikon_data
 "synthetic_eikon_data"
